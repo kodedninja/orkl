f00b0eecb520bcff4f7b6337b9977f7fd1460a91
Hunor Karamán <hunix94@gmail.com>
2018-02-27 22:14:33 +0200

    no archive text

--- src/views/no_archive.js
+++ src/views/no_archive.js
@@ -4,6 +4,10 @@ module.exports = view
 
 function view(state, emit) {
 	return html`
-		<span>p2p only</span>
+		<div>
+			<p>This is an experimental website accessible only through <a href="https://datproject.org" target="_blank">Dat</a>, a peer-to-peer data sharing protocol.</p>
+			<p>1. Download <a href="https://beakerbrowser.com" target="_blank">Beaker Browser</a></p>
+			<p>2. Navigate to <a href="${window.location.origin.replace('https://', 'dat://')}">${window.location.origin.replace('https://', 'dat://')}</a></p>
+		</div>
 	`
 }
