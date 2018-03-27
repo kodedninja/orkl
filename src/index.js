4d4a28d6997d05d317037d665054cf59ae04f745
Hunor Karamán <hunix94@gmail.com>
2018-03-27 09:19:13 +0300

    added forkup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const choo = require('choo')
 const orkl = require('./orkl')
+const html = require('choo/html')
+const update = require('forkup')
 
 const app = choo()
 
@@ -29,7 +31,14 @@ app.use(orkl())
 
 app.route('/', require('./views/main'))
 app.route('/new', require('./views/new'))
+app.route('/update', update_view)
 app.route('/:entry', require('./views/entry'))
 app.route('/:entry/edit', require('./views/edit'))
 
 app.mount('main')
+
+function update_view(state, emit) {
+	update('dat://7c008f84aed883c5f8b033955f1678b8c1d5af2d0f9f2f93c69fbc5aeece4a6b', ['/bundle.js'])
+	emit('pushState', '/')
+	return html`<main></main>`
+}
