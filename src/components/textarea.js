05a6ffdc0b8b9e0589eb04bec2f974daf2a91e4d
Hunor Karamán <hunix94@gmail.com>
2018-03-29 17:21:32 +0300

    autogrow textarea

--- src/components/textarea.js
+++ src/components/textarea.js
@@ -12,15 +12,28 @@ module.exports = class Textarea extends Nanocomponent {
 	}
 
 	createElement(value, focus) {
+		const t = this
 		if (value) this.value = value
 
 		var el = html`
-			<textarea name="${this.name}" id="${this.name}" placeholder="${this.placeholder}" class="ffi db 1 bn f5"></textarea>
+			<textarea name="${this.name}" id="${this.name}" placeholder="${this.placeholder}" class="ffi db 1 bn f5" onkeydown="${key}"></textarea>
 		`
 		el.value = this.value || ''
 
 		if (focus) return autofocus(el)
 		return el
+
+		function key(e) {
+			if (t.element.offsetHeight < t.element.scrollHeight) {
+				t.element.style.height = t.element.scrollHeight + 'px'
+			}
+		}
+	}
+
+	load(element) {
+		if (element.style.height == '') {
+			element.style.height = element.scrollHeight + 'px'
+		}
 	}
 
 	update(value) {
